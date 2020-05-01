a9aeb0bd8583243af24794f392300e3d31f772e8
Abaay918 <63595069+Abaay918@users.noreply.github.com>
2020-05-01 18:39:03 +0700

    praktikum modul 6

---
+++ Praktikum/P6_PW_193040109/Latihan6d/index.php
@@ -0,0 +1,28 @@
+<?php
+    require 'php/functions.php';
+
+    $alatmusik = query("SELECT * FROM tubes_193040109")
+
+?>
+
+<html>
+    <head>
+       
+
+        <title>19304109</title>
+        <link rel="stylesheet" type="text/css" href="Assets/css/style.css">
+
+       
+
+    </head>
+    
+    <div class="container">
+        <?php foreach ($alatmusik as $row) :?>
+            <p class="tipe">
+                 <a href="php/detail.php?id=<?= $row['id'] ?>">
+                    <?= $row["nama"] ?>
+                 </a>
+            </p>
+        <?php endforeach; ?>
+    </div>
+</html>
